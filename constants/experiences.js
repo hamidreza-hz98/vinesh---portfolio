dc1c321d82a3df8d80c4aa2cb017cbe97b54cf3b
Hamidreza Hassanzadeh <hamidrezahz1999@gmail.com>
2025-11-22 16:01:30 +0330

    remove MG projects II

--- constants/experiences.js
+++ constants/experiences.js
@@ -90,238 +90,238 @@ export const experiences = [
       },
     ],
   },
-  {
-    company: {
-      en: "Matican Group llc.",
-      pt: "Matican Group LLC",
-      fa: "شرکت Matican Group",
-    },
-    role: {
-      en: "Frontend Developer",
-      pt: "Desenvolvedor Frontend",
-      fa: "توسعه‌دهنده فرانت‌اند",
-    },
-    location: "Turkey",
-    period: {
-      from: "April 2022",
-      to: "January 2024",
-    },
-    descriptions: [
-      {
-        title: {
-          en: "About Matican",
-          pt: "Sobre a Matican",
-          fa: "درباره Matican",
-        },
-        description: {
-          en: `
-          <div>
-          <p> 
-          <a href="https://maticangroup.com/global/" target="_blank" className="font-bold"> Matican Group llc </a>
-          is a Digital Solution Provider in more than 6 countries, based in Houston, Texas, USA.
-          </p>
+//   {
+//     company: {
+//       en: "Matican Group llc.",
+//       pt: "Matican Group LLC",
+//       fa: "شرکت Matican Group",
+//     },
+//     role: {
+//       en: "Frontend Developer",
+//       pt: "Desenvolvedor Frontend",
+//       fa: "توسعه‌دهنده فرانت‌اند",
+//     },
+//     location: "Turkey",
+//     period: {
+//       from: "April 2022",
+//       to: "January 2024",
+//     },
+//     descriptions: [
+//       {
+//         title: {
+//           en: "About Matican",
+//           pt: "Sobre a Matican",
+//           fa: "درباره Matican",
+//         },
+//         description: {
+//           en: `
+//           <div>
+//           <p> 
+//           <a href="https://maticangroup.com/global/" target="_blank" className="font-bold"> Matican Group llc </a>
+//           is a Digital Solution Provider in more than 6 countries, based in Houston, Texas, USA.
+//           </p>
 
-          <p> Matican primarily focused on developing platforms aligned with business workflows, rather than merely tackling technical challenges. </p>
-          </div>
-          `,
-          pt: `
-          <div>
-          <p> 
-          <a href="https://maticangroup.com/global/" target="_blank" className="font-bold"> Matican Group llc </a>
-          é uma provedora de soluções digitais presente em mais de 6 países, sediada em Houston, Texas, EUA.
-          </p>
+//           <p> Matican primarily focused on developing platforms aligned with business workflows, rather than merely tackling technical challenges. </p>
+//           </div>
+//           `,
+//           pt: `
+//           <div>
+//           <p> 
+//           <a href="https://maticangroup.com/global/" target="_blank" className="font-bold"> Matican Group llc </a>
+//           é uma provedora de soluções digitais presente em mais de 6 países, sediada em Houston, Texas, EUA.
+//           </p>
 
-          <p> A Matican focava principalmente no desenvolvimento de plataformas alinhadas aos fluxos de trabalho empresariais, e não apenas na resolução de desafios técnicos. </p>
-          </div>
-          `,
-          fa: `
-          <div>
-          <p> 
-          <a href="https://maticangroup.com/global/" target="_blank" className="font-bold"> Matican Group llc </a>
-          یک ارائه‌دهنده راهکارهای دیجیتال در بیش از ۶ کشور است که دفتر مرکزی آن در هیوستون، تگزاس، ایالات متحده قرار دارد.
-          </p>
+//           <p> A Matican focava principalmente no desenvolvimento de plataformas alinhadas aos fluxos de trabalho empresariais, e não apenas na resolução de desafios técnicos. </p>
+//           </div>
+//           `,
+//           fa: `
+//           <div>
+//           <p> 
+//           <a href="https://maticangroup.com/global/" target="_blank" className="font-bold"> Matican Group llc </a>
+//           یک ارائه‌دهنده راهکارهای دیجیتال در بیش از ۶ کشور است که دفتر مرکزی آن در هیوستون، تگزاس، ایالات متحده قرار دارد.
+//           </p>
 
-          <p> تمرکز اصلی Matican بر توسعه پلتفرم‌هایی منطبق با جریان‌های کاری کسب‌وکارها بود، نه صرفاً حل چالش‌های فنی. </p>
-          </div>
-          `,
-        },
-      },
+//           <p> تمرکز اصلی Matican بر توسعه پلتفرم‌هایی منطبق با جریان‌های کاری کسب‌وکارها بود، نه صرفاً حل چالش‌های فنی. </p>
+//           </div>
+//           `,
+//         },
+//       },
 
-            {
-        title: {
-          en: "Collaborations",
-          pt: "Colaborações",
-          fa: "همکاری‌ها",
-        },
-        description: {
-          en: `
-          <div>
-          <p> All Collaborations in <a href="/en/works" className="font-bold"> Works </a> page are my experiences in 
-          <a href="https://maticangroup.com/global/" target="_blank" className="font-bold"> Matican Group llc </a>.</p>
-          </div>
-          `,
-          pt: `
-          <div>
-          <p> Todas as colaborações listadas na página <a href="/pt/works" className="font-bold"> Works </a> são experiências que tive na 
-          <a href="https://maticangroup.com/global/" target="_blank" className="font-bold"> Matican Group llc </a>.</p>
-          </div>
-          `,
-          fa: `
-          <div>
-          <p> تمام همکاری‌های ذکر شده در صفحه <a href="/fa/works" className="font-bold"> نمونه‌کارها </a> مربوط به تجربیات من در 
-          <a href="https://maticangroup.com/global/" target="_blank" className="font-bold"> شرکت Matican Group </a> می‌باشد.</p>
-          </div>
-          `,
-        },
-      },
-      {
-        title: {
-          en: "Internship",
-          pt: "Estágio",
-          fa: "دوره کارآموزی",
-        },
-        description: {
-          en: `
-          <div>
-          <p> I started from React.js internship in 21 days, my main task was to implement 
-              on of Matican's delivered projects in Kuwait; called <a href="https://www.bookpeep.com/english" target="_blank"> Book Peep </a>
-          </p>
-          </div>
-          `,
-          pt: `
-          <div>
-          <p> Comecei com um estágio de 21 dias em React.js. Minha principal tarefa foi implementar um dos projetos da Matican entregues no Kuwait, chamado <a href="https://www.bookpeep.com/english" target="_blank"> Book Peep </a>.</p>
-          </div>
-          `,
-          fa: `
-          <div>
-          <p> من با کارآموزی React.js به مدت ۲۱ روز شروع کردم. وظیفه اصلی‌ام پیاده‌سازی یکی از پروژه‌های تحویلی شرکت Matican در کویت با نام <a href="https://www.bookpeep.com/english" target="_blank"> Book Peep </a> بود.</p>
-          </div>
-          `,
-        },
-      },
-      {
-        title: {
-          en: "Frontend Development",
-          pt: "Desenvolvimento Frontend",
-          fa: "توسعه فرانت‌اند",
-        },
-        description: {
-          en: `
-          <div>
-          <p> Work on many technologies like Next.js, Nuxt.js, Frappe Framework,
-GraphQL, Axios, Vuetify, MUI, Tailwind CSS and Figma.  </p>
+//             {
+//         title: {
+//           en: "Collaborations",
+//           pt: "Colaborações",
+//           fa: "همکاری‌ها",
+//         },
+//         description: {
+//           en: `
+//           <div>
+//           <p> All Collaborations in <a href="/en/works" className="font-bold"> Works </a> page are my experiences in 
+//           <a href="https://maticangroup.com/global/" target="_blank" className="font-bold"> Matican Group llc </a>.</p>
+//           </div>
+//           `,
+//           pt: `
+//           <div>
+//           <p> Todas as colaborações listadas na página <a href="/pt/works" className="font-bold"> Works </a> são experiências que tive na 
+//           <a href="https://maticangroup.com/global/" target="_blank" className="font-bold"> Matican Group llc </a>.</p>
+//           </div>
+//           `,
+//           fa: `
+//           <div>
+//           <p> تمام همکاری‌های ذکر شده در صفحه <a href="/fa/works" className="font-bold"> نمونه‌کارها </a> مربوط به تجربیات من در 
+//           <a href="https://maticangroup.com/global/" target="_blank" className="font-bold"> شرکت Matican Group </a> می‌باشد.</p>
+//           </div>
+//           `,
+//         },
+//       },
+//       {
+//         title: {
+//           en: "Internship",
+//           pt: "Estágio",
+//           fa: "دوره کارآموزی",
+//         },
+//         description: {
+//           en: `
+//           <div>
+//           <p> I started from React.js internship in 21 days, my main task was to implement 
+//               on of Matican's delivered projects in Kuwait; called <a href="https://www.bookpeep.com/english" target="_blank"> Book Peep </a>
+//           </p>
+//           </div>
+//           `,
+//           pt: `
+//           <div>
+//           <p> Comecei com um estágio de 21 dias em React.js. Minha principal tarefa foi implementar um dos projetos da Matican entregues no Kuwait, chamado <a href="https://www.bookpeep.com/english" target="_blank"> Book Peep </a>.</p>
+//           </div>
+//           `,
+//           fa: `
+//           <div>
+//           <p> من با کارآموزی React.js به مدت ۲۱ روز شروع کردم. وظیفه اصلی‌ام پیاده‌سازی یکی از پروژه‌های تحویلی شرکت Matican در کویت با نام <a href="https://www.bookpeep.com/english" target="_blank"> Book Peep </a> بود.</p>
+//           </div>
+//           `,
+//         },
+//       },
+//       {
+//         title: {
+//           en: "Frontend Development",
+//           pt: "Desenvolvimento Frontend",
+//           fa: "توسعه فرانت‌اند",
+//         },
+//         description: {
+//           en: `
+//           <div>
+//           <p> Work on many technologies like Next.js, Nuxt.js, Frappe Framework,
+// GraphQL, Axios, Vuetify, MUI, Tailwind CSS and Figma.  </p>
 
-<p> I had collaborations in more that 20 projects frontend development with different scales in Matican using different technologies. </p>
-          </div>
-          `,
-          pt: `
-          <div>
-          <p> Trabalhei com várias tecnologias como Next.js, Nuxt.js, Frappe Framework,
-GraphQL, Axios, Vuetify, MUI, Tailwind CSS e Figma. </p>
+// <p> I had collaborations in more that 20 projects frontend development with different scales in Matican using different technologies. </p>
+//           </div>
+//           `,
+//           pt: `
+//           <div>
+//           <p> Trabalhei com várias tecnologias como Next.js, Nuxt.js, Frappe Framework,
+// GraphQL, Axios, Vuetify, MUI, Tailwind CSS e Figma. </p>
 
-<p> Participei do desenvolvimento frontend em mais de 20 projetos de diferentes escalas na Matican, utilizando diversas tecnologias. </p>
-          </div>
-          `,
-          fa: `
-          <div>
-          <p> با تکنولوژی‌های متنوعی مانند Next.js، Nuxt.js، فریم‌ورک Frappe،
-GraphQL، Axios، Vuetify، MUI، Tailwind CSS و Figma کار کردم. </p>
+// <p> Participei do desenvolvimento frontend em mais de 20 projetos de diferentes escalas na Matican, utilizando diversas tecnologias. </p>
+//           </div>
+//           `,
+//           fa: `
+//           <div>
+//           <p> با تکنولوژی‌های متنوعی مانند Next.js، Nuxt.js، فریم‌ورک Frappe،
+// GraphQL، Axios، Vuetify، MUI، Tailwind CSS و Figma کار کردم. </p>
 
-<p> در توسعه فرانت‌اند بیش از ۲۰ پروژه با مقیاس‌های مختلف در شرکت Matican با استفاده از تکنولوژی‌های گوناگون همکاری داشتم. </p>
-          </div>
-          `,
-        },
-      },
-      {
-        title: {
-          en: "Contribution in Meetings",
-          pt: "Participação em Reuniões",
-          fa: "مشارکت در جلسات",
-        },
-        description: {
-          en: `
-            <div>
-            <p> I needed to appear in meetings as technical team's  representative. </p>
-            </div>
-          `,
-          pt: `
-            <div>
-            <p> Eu participava das reuniões como representante da equipe técnica. </p>
-            </div>
-          `,
-          fa: `
-            <div>
-            <p> من به عنوان نماینده تیم فنی در جلسات شرکت می‌کردم. </p>
-            </div>
-          `,
-        },
-      },
-      {
-        title: {
-          en: "Responsible for Founding QA Department",
-          pt: "Responsável por Fundar o Departamento de QA",
-          fa: "مسئول تأسیس بخش کنترل کیفیت (QA)",
-        },
-        description: {
-          en: `
-            <div>
-              <p>
-               My main duty was to run Automated Quality
-Assurance test cases for <a href="https://sanacash.net/" target="_blank"> SanaCash </a> cryptocurrency exchange platform using AI automated testing tools like 
-Selenium & Appium cron jobs implementation.
-              </p>
-            </div>
-          `,
-          pt: `
-            <div>
-              <p>
-                Minha principal responsabilidade era executar testes automatizados de qualidade para a plataforma de câmbio de criptomoedas <a href="https://sanacash.net/" target="_blank"> SanaCash </a>, utilizando ferramentas de testes baseadas em IA como Selenium e Appium com implementação via cron jobs.
-              </p>
-            </div>
-          `,
-          fa: `
-            <div>
-              <p>
-                وظیفه اصلی من اجرای تست‌های خودکار تضمین کیفیت برای پلتفرم صرافی رمزارز <a href="https://sanacash.net/" target="_blank"> SanaCash </a> بود. این کار با استفاده از ابزارهای تست خودکار مبتنی بر هوش مصنوعی مانند Selenium و Appium و زمان‌بندی با کران‌جاب انجام می‌شد.
-              </p>
-            </div>
-          `,
-        },
-      },
-      {
-        title: {
-          en: "Responsive for Backend Migration R&D",
-          pt: "Responsável por Pesquisa e Migração de Backend",
-          fa: "مسئول تحقیق و توسعه مهاجرت بک‌اند",
-        },
-        description: {
-          en: `
-          <div>
-          <p>
-          Responsible for migrating the Team over to using <a href="https://frappe.io/framework" target="_blank" > Frappe framework </a> as a
-new back-end development tool. Responsible for R&D, migration and training
-colleagues.
-          </p>
-          </div>
-          `,
-          pt: `
-          <div>
-          <p>
-          Responsável por migrar a equipe para o uso do <a href="https://frappe.io/framework" target="_blank" > framework Frappe </a> como nova ferramenta de desenvolvimento backend. Atuei com pesquisa, implementação da migração e treinamento de colegas.
-          </p>
-          </div>
-          `,
-          fa: `
-          <div>
-          <p>
-          مسئول مهاجرت تیم به استفاده از <a href="https://frappe.io/framework" target="_blank" > فریم‌ورک Frappe </a> به عنوان ابزار جدید توسعه بک‌اند بودم. وظایفم شامل تحقیق و توسعه، مهاجرت و آموزش همکاران بود.
-          </p>
-          </div>
-          `,
-        },
-      },
+// <p> در توسعه فرانت‌اند بیش از ۲۰ پروژه با مقیاس‌های مختلف در شرکت Matican با استفاده از تکنولوژی‌های گوناگون همکاری داشتم. </p>
+//           </div>
+//           `,
+//         },
+//       },
+//       {
+//         title: {
+//           en: "Contribution in Meetings",
+//           pt: "Participação em Reuniões",
+//           fa: "مشارکت در جلسات",
+//         },
+//         description: {
+//           en: `
+//             <div>
+//             <p> I needed to appear in meetings as technical team's  representative. </p>
+//             </div>
+//           `,
+//           pt: `
+//             <div>
+//             <p> Eu participava das reuniões como representante da equipe técnica. </p>
+//             </div>
+//           `,
+//           fa: `
+//             <div>
+//             <p> من به عنوان نماینده تیم فنی در جلسات شرکت می‌کردم. </p>
+//             </div>
+//           `,
+//         },
+//       },
+//       {
+//         title: {
+//           en: "Responsible for Founding QA Department",
+//           pt: "Responsável por Fundar o Departamento de QA",
+//           fa: "مسئول تأسیس بخش کنترل کیفیت (QA)",
+//         },
+//         description: {
+//           en: `
+//             <div>
+//               <p>
+//                My main duty was to run Automated Quality
+// Assurance test cases for <a href="https://sanacash.net/" target="_blank"> SanaCash </a> cryptocurrency exchange platform using AI automated testing tools like 
+// Selenium & Appium cron jobs implementation.
+//               </p>
+//             </div>
+//           `,
+//           pt: `
+//             <div>
+//               <p>
+//                 Minha principal responsabilidade era executar testes automatizados de qualidade para a plataforma de câmbio de criptomoedas <a href="https://sanacash.net/" target="_blank"> SanaCash </a>, utilizando ferramentas de testes baseadas em IA como Selenium e Appium com implementação via cron jobs.
+//               </p>
+//             </div>
+//           `,
+//           fa: `
+//             <div>
+//               <p>
+//                 وظیفه اصلی من اجرای تست‌های خودکار تضمین کیفیت برای پلتفرم صرافی رمزارز <a href="https://sanacash.net/" target="_blank"> SanaCash </a> بود. این کار با استفاده از ابزارهای تست خودکار مبتنی بر هوش مصنوعی مانند Selenium و Appium و زمان‌بندی با کران‌جاب انجام می‌شد.
+//               </p>
+//             </div>
+//           `,
+//         },
+//       },
+//       {
+//         title: {
+//           en: "Responsive for Backend Migration R&D",
+//           pt: "Responsável por Pesquisa e Migração de Backend",
+//           fa: "مسئول تحقیق و توسعه مهاجرت بک‌اند",
+//         },
+//         description: {
+//           en: `
+//           <div>
+//           <p>
+//           Responsible for migrating the Team over to using <a href="https://frappe.io/framework" target="_blank" > Frappe framework </a> as a
+// new back-end development tool. Responsible for R&D, migration and training
+// colleagues.
+//           </p>
+//           </div>
+//           `,
+//           pt: `
+//           <div>
+//           <p>
+//           Responsável por migrar a equipe para o uso do <a href="https://frappe.io/framework" target="_blank" > framework Frappe </a> como nova ferramenta de desenvolvimento backend. Atuei com pesquisa, implementação da migração e treinamento de colegas.
+//           </p>
+//           </div>
+//           `,
+//           fa: `
+//           <div>
+//           <p>
+//           مسئول مهاجرت تیم به استفاده از <a href="https://frappe.io/framework" target="_blank" > فریم‌ورک Frappe </a> به عنوان ابزار جدید توسعه بک‌اند بودم. وظایفم شامل تحقیق و توسعه، مهاجرت و آموزش همکاران بود.
+//           </p>
+//           </div>
+//           `,
+//         },
+//       },
 
-    ],
-  },
+//     ],
+//   },
 ];
